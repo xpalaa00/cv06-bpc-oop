523ed6ccd74d446b7430ed4e471e06451791c944
xpalaa00 <78907070+xpalaa00@users.noreply.github.com>
2021-03-24 21:58:50 +0100

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cv06_bpc_oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
